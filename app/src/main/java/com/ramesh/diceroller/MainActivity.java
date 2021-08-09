d349b1590390155befbfc28f5a0ad9c5f6d42bdb
Ramesh-debug-lang <itsmerameshk1@gmail.com>
2021-08-10 00:59:05 +0530

    Dice Roller

--- app/src/main/java/com/ramesh/diceroller/MainActivity.java
+++ app/src/main/java/com/ramesh/diceroller/MainActivity.java
@@ -2,6 +2,7 @@ package com.ramesh.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -53,14 +54,19 @@ public class MainActivity extends AppCompatActivity {
                     roll.setEnabled(true);
                     result.setEnabled(true);
                     aSwitch.setTextOff("OFF");
-                    aSwitch.setText("OFF");
+                    aSwitch.setText("ON");
                 }else{
                     roll.setEnabled(false);
                     result.setEnabled(false);
                     aSwitch.setTextOn("ON");
-                    aSwitch.setText("ON");
+                    aSwitch.setText("OFF");
                 }
             }
         });
+    }
+
+    public void goToTwo(View view) {
+        Intent intent=new Intent(this, Two.class);
+        startActivity(intent);
     }
 }
