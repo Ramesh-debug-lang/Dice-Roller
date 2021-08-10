810105eb07d755d2d6697224c7d2f3b0d89bbcaa
Ramesh-debug-lang <itsmerameshk1@gmail.com>
2021-08-10 13:05:31 +0530

    Card

--- app/src/main/java/com/ramesh/diceroller/Birthday.kt
+++ app/src/main/java/com/ramesh/diceroller/Birthday.kt
@@ -1,7 +1,10 @@
 package com.ramesh.diceroller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,7 +16,20 @@ class Birthday : AppCompatActivity() {
         val name: TextView = findViewById(R.id.name);
         val create: Button = findViewById(R.id.create);
         create.setOnClickListener {
-            Toast.makeText(this,"Button Clicked", Toast.LENGTH_SHORT).show();
+            val message = name.text.toString();
+            if (message.length != 0) {
+                val intent = Intent(this, BCard::class.java).apply {
+                    putExtra("message", message)
+                }
+                startActivity(intent);
+            }else{
+                name.setError("Please Enter your Name")
+                Toast.makeText(this,"Please Enter your Name", Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    fun goToDiceRoller(view: View) {
+        finish();
     }
 }
